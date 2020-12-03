787a1714d3f22aaf7990fbbbe8c37aac80694356
JoiceAraujo <joicearaujo10@gmail.com>
2020-12-03 07:01:10 -0400

    [FINISHED] Initial setup local database   - Created Account methods   - Create Profile model

---
+++ lib/models/profile.dart
@@ -0,0 +1,24 @@
+class Profile {
+  int id;
+  bool main;
+  String name;
+  int accountId;
+
+  Profile.fromMap(Map<String, dynamic> map) {
+    id = map['id'];
+    name = map['name'];
+    accountId = map['account_id'];
+    main = map['main'] == 1 ? true : false;
+  }
+
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = {
+      'id': id,
+      'name': name,
+      'account_id': accountId,
+      'main': main == true ? 1 : 0,
+    };
+
+    return map;
+  }
+}
