bae09e6552ab3283f61830f75775bfc94d9dd1cd
JoiceAraujo <joicearaujo10@gmail.com>
2020-12-10 08:40:55 -0400

    [WIP] Create Profile screen  - Created ChangeNotifierProvider<Profile Controller>  - Created Sign Out functionality

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,4 +1,6 @@
+import 'package:consume_marvel_api/controllers/account_controller.dart';
 import 'package:consume_marvel_api/controllers/profile_controller.dart';
+import 'package:consume_marvel_api/models/profile.dart';
 import 'package:consume_marvel_api/widgets/elevated_button_custom.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -8,11 +10,16 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final List<Profile> profileList = Provider.of<ProfileController>(context).
+    final ctrlProfile = Provider.of<ProfileController>(context);
+    final accountController =
+        Provider.of<AccountController>(context, listen: false);
+    final List<Profile> profileList = ctrlProfile.profilesList;
 
     return Column(
       children: [
-        ElevatedButtonCustom('titleButton', () {}),
+        ElevatedButtonCustom('Sign Out', () {
+          accountController.signOut();
+        }),
         ListView(),
       ],
     );
